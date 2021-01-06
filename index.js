2fae977cc055ae82e97701103d4e9dcc91d42661
Saumya2496 <70735891+Saumya2496@users.noreply.github.com>
2021-01-06 11:21:59 +0530

    part of basic website

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <div>
+  <h1> Welcome To Our Website </h1>
+  <p>List of 5 Best Series::-</p>
+  <ol>1.Dark
+  2.The family Man <br/>
+  3.Jack Ryan<br/>
+  4.Hunter<br/>
+  5.Descendants of the sun.<br/></ol>
+  
+  </div>,
+  document.getElementById('root')
+);
