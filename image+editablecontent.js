a8e5c8f495b5ad54c8cf1f9977cd660a353c1cab
Saumya2496 <70735891+Saumya2496@users.noreply.github.com>
2021-01-07 07:34:21 +0530

    Add files via upload

---
+++ image+editablecontent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name= "saumya sharma";
+const img1= "https://picsum.photos/400/400" ;
+const img2= "https://picsum.photos/seed/picsum/400/400";
+const img3="https://picsum.photos/id/237/400/400" ;
+
+ReactDOM.render(
+  <>
+  <h1 contentEditable = "true">Hi welcome to {name}'s gallery</h1>
+  <img src= {img1} />
+  <img src= {img2} />
+  <img src= {img3} />
+</>, document.getElementById("root")
+);
+
